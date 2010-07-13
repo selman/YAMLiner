8bdee6b994047ffba1afc9b2492ab260f25bdf37
Selman ULUG <selman.ulug@gmail.com>
2010-07-14 02:56:32 +0300

    metaprogramming tries

--- lib/yamliner.rb
+++ lib/yamliner.rb
@@ -1,110 +1,115 @@
 #Defines YAMLiner class
-require 'yaml'
-require 'fileutils'
+#require 'yaml'
+#require 'fileutils'
 
+module YAMLiner
+  def self.line(*objects)
+    objects.each do |object|
+      
+      #YAMLiner class definition
+      class << object
+        require 'yaml'
+        require 'fileutils'
 
-#YAMLiner class definition
-class YAMLiner
-  #Our accessor to carry values between function [] and []= redifined
-  #you can access all params like this:
-  #  y = YAMLiner.new()
-  #  y[:name] = "my_comment"
-  attr_accessor :params
+        attr_accessor_with_default_setter :params do
+          { :name => 'YAMLiner',
+            :file => '',
+            :line => 0,
+            :prefix => '#',
+            :postfix => '',
+            :backup => false } 
+        end
 
-  #Our initializer
-  def initialize(params = {})
-    @params = {
-      :name => 'YAMLiner',
-      :file => '',
-      :line => 0,
-      :object => nil,
-      :prefix => '#',
-      :postfix => '',
-      :backup => false
-    }
-    @params.merge!(params) unless params.empty?
-    @file = []
-    check_params
-    @match_line = %r/(^#{Regexp.escape(@params[:prefix] + @params[:name])})(.*?)(#{Regexp.escape(@params[:postfix])}$)/
+        attr_accessor_with_default_setter :file do
+          []
+        end
+
+        def match_line
+          %r/(^#{Regexp.escape(@params[:prefix] + @params[:name])})(.*?)(#{Regexp.escape(@params[:postfix])}$)/
+        end
+
+        #Reads your supplied file/files if successfull returns readed object 
+        #  Dir['**/*.txt].each {|f| y.read }
+        def yamline_read
+          return unless read_file?
+          @file.each do |rline|
+            if rline =~ match_line
+              return YAML::load($2)
+            end
+          end
+          nil
+        end
+
+        #Writes the generated YAMLiner line to supplied file/files if there
+        #is a same formated line it writes over it
+        def yamline_write!
+          read_file?
+#          return if @file.size < params[:line]
+          temp = []
+          @file.each do |wline|
+            if @params[:line] == @file.index(wline)
+              wline =~ match_line ? wline = yamline : temp << yamline
+            end
+            temp << wline
+          end
+          save_file(temp)
+        end
+
+        #Finds and deletes formatted line from supplied file/files
+        def yamline_delete!
+          return unless read_file?
+          temp = []
+          @file.each do |dline|
+            temp << dline unless dline =~ match_line
+          end
+          save_file(temp)
+        end
+
+        #Returns generated YAMLiner line
+        def yamline
+          #You can get inline YAML only redefining *to_yaml_style* method
+          instance_eval { def to_yaml_style; :inline; end }
+          @params[:prefix] + @params[:name] + self.to_yaml.chop + @params[:postfix] + "\n"
+        end
+
+        private
+
+        def read_file?
+          return if @params[:file].empty?
+          return unless File.exists?(@params[:file])
+          @file = File.readlines(@params[:file])
+        end
+
+        def save_file(temp)
+          @params[:backup] ? file = "#{@params[:file]}.bak" : file = @params[:file]
+          File.open(file, 'w+') { |f| f.puts temp }
+          read_file?
+        end
+
+      end # YAMLiner
+
+    end
   end
+end
 
-  #Reads your supplied file/files if successfull returns readed object 
-  #  Dir['**/*.txt].each {|f| y.read }
-  def read
-    check_params [:file]
-    return false if @file.empty?
-    @file.each do |rline|
-      if rline =~ @match_line
-        return YAML::load($2)
+class Module
+  def attr_accessor_with_default_setter( *syms, &block )
+    raise 'Default value in block required' unless block
+    syms.each do | sym |
+      module_eval do
+        attr_writer( sym )
+        define_method( sym ) do | |
+          class << self; self; end.class_eval do
+            attr_reader( sym )
+          end
+          if instance_variables.include? "@#{sym}"
+            instance_variable_get( "@#{sym}" )
+          else
+            instance_variable_set( "@#{sym}", block.call )
+          end
+        end
       end
     end
-    return false
+    nil
   end
-
-  #Writes the generated YAMLiner line to supplied file/files if there
-  #is a same formated line it writes over it
-  def write!
-    check_params [:file, :object]
-    return false if @file.size < params[:line]
-    temp = []
-    @file.each do |wline|
-      if @params[:line] == @file.index(wline)
-        wline =~ @match_line ? wline = yamline : temp << yamline
-      end
-      temp << wline
-    end
-    save_file(temp)
-  end
-
-  #Finds and deletes formatted line from supplied file/files
-  def delete!
-    check_params [:file]
-    return false if @file.empty?
-    temp = []
-    @file.each do |dline|
-      temp << dline unless dline =~ @match_line
-    end
-    save_file(temp)
-  end
-
-  #Returns generated YAMLiner line
-  def yamline
-    #You can get inline YAML only redefining *to_yaml_style* method
-    @params[:object].instance_eval { def to_yaml_style; :inline; end }
-    @params[:prefix] + @params[:name] + @params[:object].to_yaml.chop + @params[:postfix] + "\n"
-  end
-
-  #Shortcut to read params
-  def [](key)
-    @params[key]
-  end
-
-  #Shortcut to write params
-  def []=(key, value)
-    @params[key] = value
-  end
-
-  private
-
-  def read_file?
-    return false if @params[:file].empty?
-    return false unless File.exists?(@params[:file])
-    @file = File.readlines(@params[:file])
-  end
-
-  def save_file(temp)
-    @params[:backup] ? file = "#{@params[:file]}.bak" : file = @params[:file]
-    File.open(file, 'w+') { |f| f.puts temp }
-    read_file?
-  end
-
-  def check_params(params = [])
-    params += [:name, :prefix]
-    params.each do |opt|
-      raise ArgumentError.new("\":#{opt}\" option must be set") if @params[opt].nil? or @params[opt].empty?
-    end
-    read_file? if params.include?(:file)
-  end
-
-end # YAMLiner
-
+end
--- spec/yamliner_spec.rb
+++ spec/yamliner_spec.rb
@@ -8,45 +8,46 @@ describe "Yamliner" do
       f.puts "Test File"
     end
     @input = {:name => 'selman', :surname => 'ulug'}
-    @y = YAMLiner.new(:object => @input)
+    YAMLiner::line @input
   end
 
-  it "should raise ArgumentError without :name or :prefix parameters" do
-    lambda { YAMLiner.new :name => '' }.should raise_exception(ArgumentError)
-    lambda { YAMLiner.new :prefix => nil }.should raise_exception(ArgumentError)
-    lambda { YAMLiner.new }.should_not raise_exception(ArgumentError)
-  end
+  # it "should raise ArgumentError without :name or :prefix parameters" do
+  #   lambda { YAMLiner.new :name => '' }.should raise_exception(ArgumentError)
+  #   lambda { YAMLiner.new :prefix => nil }.should raise_exception(ArgumentError)
+  #   lambda { YAMLiner.new }.should_not raise_exception(ArgumentError)
+  # end
 
-  it "should raise Argument error when no file specified to read" do
-    lambda { @y.read }.should raise_exception(ArgumentError)
-  end
+#  it "should raise Argument error when no file specified to read" do
+#    @input.yamline_read.should raise_exception
+#  end
 
   it "should return false when specified file is not available to read" do
-    @y[:file] = 'not_available.txt'
-    @y.read.should be_false
+    @input.params[:file] = 'not_available.txt'
+    @input.yamline_read.should be_false
   end
 
   it "should read specified file and return false when no YAMLiner" do
-    @y[:file] = @test_file
-    @y.read.should be_false
+    @input.params[:file] = @test_file
+    @input.yamline_read.should be_false
   end
 
   it 'should write to file' do
-    @y.write!.should be_true
+    @input.yamline_write!.should be_true
   end
 
   it "should read file and two objects must be equal" do
-    @y.read.should_not be_false
-    @y[:object].size.should eql(@input.size)
+    @input.yamline_read.should_not be_false
+    o = @input.yamline_read
+    o.size.should eql(@input.size)
     @input.each do |k, v|
-      @y[:object].has_key?(k).should be_true
-      @y[:object].has_value?(v).should be_true
+      o.has_key?(k).should be_true
+      o.has_value?(v).should be_true
     end
   end
 
   it 'should delete from file' do
-    @y.delete!.should be_true
-    @y.read.should be_false
+    @input.yamline_delete!.should be_true
+    @input.yamline_read.should be_false
   end
 
   after(:all) do
